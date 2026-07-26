0b67d3b8df888b228ce2df2c9a44c0698c3d8718
pulga <34066936+gatomontes@users.noreply.github.com>
2026-07-26 12:22:22 -0400

    Connect response delivery to reference facade

--- src/reference.ts
+++ src/reference.ts
@@ -1,4 +1,5 @@
 import { ArtifactEnvelope } from "./artifact.js";
+import { ResponseDelivery, ResponseDeliveryService } from "./delivery.js";
 import { Castellan, WorkSpecification } from "./castellan.js";
 import { OperatorRequest, Petition, Secretariat } from "./secretariat.js";
 
@@ -6,7 +7,26 @@ export class ImperiumReference {
   constructor(
     private readonly secretariat = new Secretariat(),
     private readonly castellan = new Castellan(),
+    private readonly delivery = new ResponseDeliveryService(),
   ) {}
+
+  prepareResponse(
+    petition: ArtifactEnvelope<Petition>,
+    channel: string,
+  ): ArtifactEnvelope<ResponseDelivery> {
+    return this.delivery.prepare(
+      "response-for-" + petition.identity + "@" + petition.version,
+      petition.correlationId,
+      channel,
+    );
+  }
+
+  dispatchResponse(
+    delivery: ArtifactEnvelope<ResponseDelivery>,
+    successful: boolean,
+  ): ArtifactEnvelope<ResponseDelivery> {
+    return this.delivery.dispatch(delivery, successful);
+  }
 
   submit(request: OperatorRequest): {
     petition: ArtifactEnvelope<Petition>;
