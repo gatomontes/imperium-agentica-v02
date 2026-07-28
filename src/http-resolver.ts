6b9590c14a5d80e93c7555ebab9345d374b5e6a1
pulga <34066936+gatomontes@users.noreply.github.com>
2026-07-28 18:51:20 -0400

    Allow asynchronous HTTP artifact resolution

--- src/http-resolver.ts
+++ src/http-resolver.ts
@@ -3,6 +3,6 @@ import { ResponseDelivery } from "./delivery.js";
 import { Petition } from "./secretariat.js";
 
 export interface HttpArtifactResolver {
-  resolvePetition(ref: string): ArtifactEnvelope<Petition> | undefined;
-  resolveDelivery(ref: string): ArtifactEnvelope<ResponseDelivery> | undefined;
+  resolvePetition(ref: string): ArtifactEnvelope<Petition> | Promise<ArtifactEnvelope<Petition> | undefined>;
+  resolveDelivery(ref: string): ArtifactEnvelope<ResponseDelivery> | Promise<ArtifactEnvelope<ResponseDelivery> | undefined>;
 }
